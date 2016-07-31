2e2354a07fb9a4e3c010240adea67457bfbe68fc
Simon Gonzalez <ximonali@gmail.com>
2016-07-31 12:28:05 -0400

    Layout
    -Adding Views

---
+++ notesApp/ViewController2.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController2.swift
+//  notesApp
+//
+//  Created by Simon Gonzalez on 2016-07-31.
+//  Copyright © 2016 skl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
